7acd1fc299558e83a004829c31054410db98d329
Andrew Randan <andrewrandan@gmail.com>
2025-06-16 21:01:01 +0300

    feat(DART-06): mathOperators

--- 1_task.dart
+++ 1_task.dart
@@ -1,1 +1,18 @@
-main() {}
+main() {
+  mathOperators();
+}
+
+void mathOperators() {
+  int a = 10;
+  int b = 20;
+
+  int perimeter = 2 * (a + b);
+  int area = a * b;
+
+  print('Perimeter: $perimeter');
+  print('Area: $area');
+
+  int sum1Area = area ~/ 1;
+
+  print('sum 1x1 square: $sum1Area');
+}
