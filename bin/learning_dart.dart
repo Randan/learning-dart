afc79511624853dd4362b174a4e9d8e1f8750e4c
Andrew Randan <andrewrandan@gmail.com>
2025-07-10 19:17:17 +0300

    feat: enable execution of lessons 6, 8, and 9 in main function

--- bin/learning_dart.dart
+++ bin/learning_dart.dart
@@ -1,11 +1,11 @@
-// import 'package:learning_dart/lesson_6/main.dart';
-// import 'package:learning_dart/lesson_8/main.dart';
-// import 'package:learning_dart/lesson_9/main.dart';
+import 'package:learning_dart/lesson_6/main.dart';
+import 'package:learning_dart/lesson_8/main.dart';
+import 'package:learning_dart/lesson_9/main.dart';
 import 'package:learning_dart/lesson_11/main.dart';
 
 void main(List<String> arguments) {
-  // lesson_6();
-  // lesson_8();
-  // lesson_9();
+  lesson_6();
+  lesson_8();
+  lesson_9();
   lesson_11();
 }
